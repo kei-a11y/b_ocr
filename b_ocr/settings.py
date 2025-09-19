f383f904310040a27204111e66f3c34c72bf53d2
kei-a11y <kei.programing@gmail.com>
2025-09-19 19:13:24 +0900

    Update settings.py

--- b_ocr/settings.py
+++ b_ocr/settings.py
@@ -122,6 +122,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # 静的ファイルの設定
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / 'static']  
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
